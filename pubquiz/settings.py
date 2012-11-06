31477551cd3ba388ff3f254a72266d6d5e92ca73
Selena Deckelmann <selenamarie@gmail.com>
2012-11-05 20:51:02 -0800

    Initial commit

--- pubquiz/settings.py
+++ pubquiz/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pubquiz',                      # Or path to database file if using sqlite3.
+        'NAME': '/tmp/pubquiz',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'registration',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -149,3 +150,5 @@ LOGGING = {
         },
     }
 }
+
+ACCOUNT_ACTIVATION_DAYS = 1
--- pubquiz/urls.py
+++ pubquiz/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic.simple import direct_to_template
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    # using django-registration
+    (r'', include('registration.backends.default.urls')),
+
 )
